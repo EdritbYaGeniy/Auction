aa065141342da193e4ea0398150eeb855711e675
Anatoliy.Gugiyev <146279154+EdritbYaGeniy@users.noreply.github.com>
2025-12-16 17:54:24 +0600

    Update 2025_12_16_113622_create_file_for_lots_table.php

--- src/database/migrations/2025_12_16_113622_create_file_for_lots_table.php
+++ src/database/migrations/2025_12_16_113622_create_file_for_lots_table.php
@@ -11,9 +11,19 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('file_for_lots', function (Blueprint $table) {
+        Schema::create('file_for_lots', function(Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->string('name', 255);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->integer('user_id');
+            $table->integer('auction_id');
+            $table->integer('lot_id');
+            $table->integer('bid_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('auction_id')->references('id')->on('auctions')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('lot_id')->references('id')->on('lots')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('bid_id')->references('id')->on('bids')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
@@ -22,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('file_for_lots');
+        //
     }
 };
